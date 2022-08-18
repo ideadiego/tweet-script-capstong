59bc66610b413618b96a1df9faa08026ed2a297a
ideadiego <dvalenzuela3@uc.cl>
2022-08-17 21:11:53 -0400

    most retweeted done

--- top-tweets.py
+++ top-tweets.py
@@ -2,9 +2,15 @@ import json
 def top_tweets():
     with open('farmers-protest-tweets-2021-03-5.json', 'r') as data:
         entries = []
-        for i in range(3):
-            entries.append(json.loads(data.readline()))
-    print(entries[0]['url'])
+        for line in data.readlines():
+            entries.append(json.loads(line))
+    max_count = 0
+    url = ""
+    for entrie in entries:
+        if entrie['retweetCount'] > max_count:
+            url = entrie["url"]
+            max_count = entrie['retweetCount']
+    print(f'url: {url}, retweets: {max_count}')
 
 if __name__ == "__main__":
     top_tweets()
