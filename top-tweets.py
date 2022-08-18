462f127d3150199a929ababb091ff3e86a1c4657
ideadiego <dvalenzuela3@uc.cl>
2022-08-17 21:04:04 -0400

    lectura de json en top tweets

--- top-tweets.py
+++ top-tweets.py
@@ -1,2 +1,10 @@
+import json
 def top_tweets():
-    pass
+    with open('farmers-protest-tweets-2021-03-5.json', 'r') as data:
+        entries = []
+        for i in range(3):
+            entries.append(json.loads(data.readline()))
+    print(entries[0]['url'])
+
+if __name__ == "__main__":
+    top_tweets()
